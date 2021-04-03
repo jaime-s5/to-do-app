636da34e915fa1deb10794be86a3446966923314
jaime-s5 <81193092+jaime-s5@users.noreply.github.com>
2021-04-03 22:51:52 +0200

    Todos and Project Manager functionality

---
+++ src/scripts/modules/project_manager.js
@@ -0,0 +1,71 @@
+import _ from 'lodash';
+
+function Todo(title, description, dueDate, priority, project) {
+  return { title, description, dueDate, priority, project };
+}
+
+function Project(name) {
+  const title = name;
+  const todos = [];
+
+  function addTodos(...todoArray) {
+    todos.push(...todoArray);
+  }
+
+  function removeTodo(index) {
+    todos.splice(index, 1);
+  }
+
+  function replaceTodo(index, todo) {
+    todos.splice(index, 1, todo);
+  }
+
+  function getMatchedTodo(index) {
+    return [_.clone(todos[index])];
+  }
+
+  function getTodos() {
+    return _.clone(todos);
+  }
+
+  const proto = Object.create({
+    addTodos,
+    removeTodo,
+    replaceTodo,
+    getTodos,
+    getMatchedTodo,
+  });
+
+  return Object.assign(proto, { title });
+}
+
+const ProjectManager = (function () {
+  const projects = [];
+
+  function addProject(project) {
+    projects.push(project);
+  }
+
+  function removeProject(index) {
+    projects.slice(index, 1);
+  }
+
+  function retrieveData(data) {
+    projects.push(data);
+  }
+
+  function cloneData() {
+    return _.cloneDeep(projects);
+  }
+
+  const proto = Object.create({
+    addProject,
+    removeProject,
+    retrieveData,
+    cloneData,
+  });
+
+  return Object.assign(proto, { projects });
+})();
+
+export { Todo, Project, ProjectManager };
