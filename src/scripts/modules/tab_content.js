0e365fa13765382ba92572123bb3ef3ed255df7a
jaime-s5 <81193092+jaime-s5@users.noreply.github.com>
2021-06-05 19:43:55 +0200

    Today and Week Tabs functionality

--- src/scripts/modules/tab_content.js
+++ src/scripts/modules/tab_content.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { isToday, isThisWeek, parseISO } from 'date-fns';
 
 import getTodoCardHTML from './interface/todo_card';
 import { Todo, Project, ProjectManager } from './project_manager';
@@ -9,21 +10,21 @@ import { generateTodoHover, handleCardEvents } from './todo';
     Todo(
       'Wash teeth',
       'Some brush to wash your teeth, bought for 23.99$ at the Lidl Supermarket',
-      '2021-12-19',
+      '2021-06-05',
       'Low',
       'Inbox'
     ),
     Todo(
       'Homework',
       'Calculos homework chapter 2',
-      '2021-12-19',
+      '2021-06-06',
       'High',
       'Inbox'
     ),
     Todo(
       'Homework',
       'Algebra homework chapter',
-      '2021-12-19',
+      '2021-06-05',
       'High',
       'Homework'
     ),
@@ -77,13 +78,37 @@ function generateTabAddTodo() {
     .addEventListener('click', () => generateTodoHover());
 }
 
-// TODO: Extract todos from single function and pass array to generateTabCards
-function generateTodayTabContent() {
-  generateTabCards([ProjectManager.projects[0].getMatchedTodo(0)]);
+// Checks for matched TODOs according the checker callback
+// (isToday, isThisWeek)
+function matchTodosDate(isMatch) {
+  const projects = _.cloneDeep(ProjectManager.projects);
+  const matchedTodos = [];
+
+  for (let i = 0; i < projects.length; i++) {
+    const todos = projects[i].getTodos();
+    for (let j = 0; j < todos.length; j++) {
+      const dueDate = parseISO(todos[j].dueDate);
+      if (isMatch(dueDate)) {
+        const todo = { project_id: i, todo_id: j, todoObject: todos[j] };
+        matchedTodos.push(todo);
+      }
+    }
+  }
+
+  return matchedTodos;
 }
 
+// Display todos which dueDate is today
+function generateTodayTabContent() {
+  const matchedTodos = matchTodosDate(isToday);
+
+  generateTabCards(matchedTodos);
+}
+// Display todos which dueDate is this week
 function generateWeekTabContent() {
-  generateTabCards([ProjectManager.projects[0].getMatchedTodo(1)]);
+  const matchedTodos = matchTodosDate(isThisWeek);
+
+  generateTabCards(matchedTodos);
 }
 
 // Inbox and Projects Tabs
@@ -97,7 +122,7 @@ function generateProjectTabContent(projectTitle) {
 
   const matchedTodos = [];
   const todos = project.getTodos();
-  // TODO: Mira si se puede hacer con un map
+
   for (let i = 0; i < todos.length; i++) {
     const todo = { project_id: indexProject, todo_id: i, todoObject: todos[i] };
     matchedTodos.push(todo);
